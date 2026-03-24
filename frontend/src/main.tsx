83d1d0cacec7506f8f2bb7479470dedea38cdbdb
EC2 Default User <ec2-user@ip-10-0-1-208.ec2.internal>
2026-03-24 18:18:55 +0000

    fix: use aws-amplify umbrella package for proper auth token provider registration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Amplify } from "@aws-amplify/core";
+import { Amplify } from "aws-amplify";
 import App from "./App";
 import { config } from "./config";
 import "./styles/global.css";
